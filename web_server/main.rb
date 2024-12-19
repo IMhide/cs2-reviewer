52f3e8bbf841b40f4acbac9f00ff6f1d89c181a6
Sam Ettahiri <sam@baijobu.com>
2024-12-19 14:38:38 +0100

    Separating test route from upload route for easy work

--- web_server/main.rb
+++ web_server/main.rb
@@ -31,8 +31,31 @@ get '/' do
 end
 
 post '/upload' do
-  params[:file][:tempfile]
-  # stdout, _stderr, _status = Open3.capture3('../out/bin/cs2-reviewer', tempfile.path)
+  tempfile = params[:file][:tempfile]
+  stdout, _stderr, _status = Open3.capture3('../out/bin/cs2-reviewer', tempfile.path)
+  json = Oj.load(stdout)
+
+  @demoInfo = DemoInfo.new(**json['demoInfo'])
+  @teams = []
+  @teams[0] = Team.new(json['teams'][0]['id'], json['teams'][0]['name'])
+  @teams[1] = Team.new(json['teams'][1]['id'], json['teams'][1]['name'])
+
+  @teams[0].players = json['teams'][0]['players'].map do |player|
+    Player.new(**player)
+  end
+  @teams[1].players = json['teams'][1]['players'].map do |player|
+    Player.new(**player)
+  end
+
+  @rounds = json['rounds'].map do |round|
+    Round.new(round['roundNumber'], round['startTime'], round['endTime'], round['winningReason'],
+      round['winnerState']['id'], round['loserState']['id'], round['killFeed'] || [])
+  end
+
+  haml :upload, format: :html5
+end
+
+get '/test' do
   stdout = File.read('tmp/de_nuke.json')
   json = Oj.load(stdout)
 
